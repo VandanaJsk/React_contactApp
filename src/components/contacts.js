9a3157e4754cd0df930bcfe56293364bdb463a64
Vandana Maurya <vandanamaurya@gmail.com>
2022-06-03 18:11:17 -0700

    react project

---
+++ src/components/contacts.js
@@ -0,0 +1,25 @@
+const contacts=[
+    {
+        name:"Bill Gates",
+        imgURL:
+        "https://pbs.twimg.com/profile_images/1414439092373254147/JdS8yLGI_400x400.jpg",
+        phone:"+123 456 789",
+        email:"billgates@gmail.com"
+    },
+    {
+        name:"Alon Musk",
+        imgURL:
+        "https://i.guim.co.uk/img/media/946b1366d7296ef755c1c33b6baf58fe8641b95b/0_102_3500_2100/master/3500.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=7403be0738d40213aa807485b78df394",
+        phone:"+753 456 0789",
+        email:"alonmusk@gmail.com"
+    },
+    {
+        name:"Steve Jobs",
+        imgURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/800px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg" ,
+        phone:"+893 456 7809",
+        email:"stevejobs@gmail.com"
+    }
+    ];
+    
+    export default contacts;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+// function Card(props){
+//   return(
+//      <div className='my-style'>
+//      <h2>{props.name}</h2>
+//     <img 
+//     src= {props.img}
+//     alt="avatar_img" 
+//     />
+//     <p>{props.tel}</p>
+//     <p>{props.email}</p>
+//   </div>
+//   );
+// }
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+//    <div>
+//  <h1>My Contacts</h1>
+//    <Card
+//   name="Bill Gates" 
+//   img="https://pbs.twimg.com/profile_images/1414439092373254147/JdS8yLGI_400x400.jpg" 
+//   tel="+123 456 789"
+//   email="b@billgates.com"
+//    />
+//   <Card
+//   name="Alon Musk"
+// img="https://i.guim.co.uk/img/media/946b1366d7296ef755c1c33b6baf58fe8641b95b/0_102_3500_2100/master/3500.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=7403be0738d40213aa807485b78df394"
+// tel="+234 650 789"
+// email="a@alonmusk.com"
+//   />
+
+//   <Card
+//   name="Steve Jobs"
+//   img="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/800px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg" 
+//   tel="+876 453 120"
+//   email="s@stevejobs.com"
+//   /> 
+//    </div>,
+//   document.getElementById("root")
+// );
+
+
+
